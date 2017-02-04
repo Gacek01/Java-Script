6690bf3b4a30a48dce0c2de061f7aa4d01a6bef3
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2017-02-04 11:53:15 +0100

    loops

---
+++ js/7-loops.js
@@ -0,0 +1,18 @@
+'use strict';
+//Loop For//
+for (var i = 0; i < 10; i++) {
+	//console.log(i)//;
+}
+/*postimlementacja vs preimplementacja */
+//Loop While//
+var it = 0;
+while (it < 10) {
+	//console.log(it);//
+	it++;
+}
+//Loop Do While//
+var iter = 20;
+do {
+	//	console.log(iter);
+	//	iter++;
+} while (iter < 10)
