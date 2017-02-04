a63b5b754ad731e002f5d5b30e304b8a19d85879
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2017-02-04 15:27:36 +0100

    variables

---
+++ js/10-variable-scope.js
@@ -0,0 +1,15 @@
+'use strict';
+//never use this code :) //
+var iloscOsoba1 = 3500
+	, iloscOsoba2 = 3000
+	, iloscOsoba3 = 5000
+	, suma;
+
+function policzsume(iloscOsoba1, iloscOsoba2, iloscOsoba3) {
+	var suma;
+	suma = iloscOsoba1 + iloscOsoba2 + iloscOsoba3 + iloscOsoba4;
+	var osoba = [suma, wiek]
+	return osoba;
+}
+console.log(policzsume(iloscOsoba3, iloscOsoba2, suma, 25))
+iloscOsoba4 = 15000;
--- js/9-functions.js
+++ js/9-functions.js
@@ -1,21 +1,22 @@
 'use strict';
-var liczba1 = 8
-	, liczba2 = -10
-	, liczba3 = -15
+var l1 = -10
+	, l2 = -101
+	, l3 = 10
+	, l4 = -345
+	, l5 = 3453
+	, l6 = 234
+	, l7 = -45
+	, l8 = 1207
+	, l9 = 12083
 	, wynik;
 
 function mnozenie(par1, par2, par3) {
-	wynik = par1 * par2 * par3;
-	return wynik;
+	return par1 * par2 * par3;
 }
-mnozenie(liczba1, liczba2, liczba3);
 
-function check(par1) {
-	if (par1 >= 0) {
-		console.log(wynik);
-	}
-	else {
-		document.write(wynik);
-	}
+function check(wynik) {
+	(wynik >= 0) ? console.log(wynik): document.write(wynik);
 }
-check(wynik);
+check(mnozenie(l1, l2, l3));
+check(mnozenie(l4, l5, l6));
+check(mnozenie(l7, l8, l9));
